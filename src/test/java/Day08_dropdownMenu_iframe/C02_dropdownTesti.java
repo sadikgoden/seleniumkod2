65248cf2cf47366c93e1e5a80fadb4eebe1d6030
sadik Goden <sadikgoden001@gmail.com>
2023-09-30 15:59:03 +0300

    cde

--- src/test/java/Day08_dropdownMenu_iframe/C02_dropdownTesti.java
+++ src/test/java/Day08_dropdownMenu_iframe/C02_dropdownTesti.java
@@ -1,5 +1,49 @@
 package Day08_dropdownMenu_iframe;
 
-public class C02_dropdownTesti {
-    
-}
+import Utilities.TestBase_BeforeClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
+public class C02_dropdownTesti extends TestBase_BeforeClass {
+   WebElement kategoriDropdownElementi;
+    Select select;
+   @Before
+   public void amazonGidis(){
+       //● https://www.amazon.com/ adresine gidin.
+       driver.get("https://www.amazon.com/");
+       kategoriDropdownElementi= driver.findElement(By.xpath("//*[@id='searchDropdownBox']"));
+       select = new Select(kategoriDropdownElementi);
+    }
+
+    @Test
+    public void dropdownTesti(){
+        //- Test 1
+        //  Arama kutusunun yanindaki kategori menusundeki kategori sayisinin 45 oldugunu test edin
+        List<WebElement> optionsList = select.getOptions();
+        int expectedKategoriSayisi= 45;
+        int actualKategoriListesi = optionsList.size();
+        Assert.assertEquals("Kategori dropdown menusunde 45 opsiyon yok",expectedKategoriSayisi,actualKategoriListesi);
+    }
+    @Test
+    public void JavaAramaTesti(){
+        //-Test 2
+        //  1. Kategori menusunden Books secenegini  secin
+        select.selectByVisibleText("Books");
+        //  2. Arama kutusuna Java yazin ve aratin
+        WebElement aramaKutusu = driver.findElement(By.id("twotabsearchtextbox"));
+        aramaKutusu.sendKeys("java"+ Keys.ENTER);
+        //  3. Bulunan sonuc sayisini yazdirin
+        WebElement aramaSonucElementi = driver.findElement(By.xpath("(//div[@class='a-section a-spacing-small a-spacing-top-small'])[1]"));
+        System.out.println(aramaSonucElementi.getText());
+
+    }
+    }
+
+
