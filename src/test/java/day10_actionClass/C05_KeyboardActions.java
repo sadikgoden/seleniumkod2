26eed3b174b245a161647754f14d5e5080d35510
sadik Goden <sadikgoden001@gmail.com>
2023-10-06 16:15:58 +0300

    qwerffffccccct

--- src/test/java/day10_actionClass/C05_KeyboardActions.java
+++ src/test/java/day10_actionClass/C05_KeyboardActions.java
@@ -1,26 +1,28 @@
 package day10_actionClass;
 
 import Utilities.TestBase;
+import com.github.javafaker.Faker;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 public class C05_KeyboardActions extends TestBase {
-
-
-
-}
-/*
- @Test
+    @Test
     public void test01(){
         //1- https://www.facebook.com adresine gidelim
         driver.get("https://www.facebook.com");
         //2- Cookies kabul edin
-        driver.findElement(By.xpath("//button[@title='Allow all cookies']")).click();
+       // driver.findElement(By.xpath("//button[@title='Allow all cookies']")).click();
+
         //3- Yeni hesap olustur butonuna basalim
         driver.findElement(By.xpath("//*[@*='open-registration-form-button']")).click();
         //4- Ad, soyad, mail ve sifre kutularina deger yazalim ve kaydol tusuna basalim
-        WebElement isimKutusu= driver.findElement(By.name("firstname"));
+        WebElement isimKutusu = driver.findElement(By.xpath("//*[@name='firstname']"));
         Actions actions = new Actions(driver);
-        Faker faker = new Faker();
-        String email= faker.internet().emailAddress();
+        Faker faker =new Faker();
+        String email = faker.internet().emailAddress();
         actions.click(isimKutusu)
                 .sendKeys(faker.name().firstName())
                 .sendKeys(Keys.TAB)
@@ -28,20 +30,22 @@ public class C05_KeyboardActions extends TestBase {
                 .sendKeys(Keys.TAB)
                 .sendKeys(email)
                 .sendKeys(Keys.TAB)
-                .sendKeys(email)
-                .sendKeys(Keys.TAB)
                 .sendKeys(faker.internet().password())
                 .sendKeys(Keys.TAB)
                 .sendKeys(Keys.TAB)
-                .sendKeys("5")
+                .sendKeys("17")
                 .sendKeys(Keys.TAB)
-                .sendKeys("Oct")
+                .sendKeys("Haz")
                 .sendKeys(Keys.TAB)
-                .sendKeys("2000")
+                .sendKeys("1983")
                 .sendKeys(Keys.TAB)
                 .sendKeys(Keys.TAB)
                 .sendKeys(Keys.ARROW_RIGHT)
                 .perform();
+        bekle(6);
+        //5- Kaydol tusuna basalim
+        driver.findElement(By.name("websubmit")).click();
+}}/*
         //5- Kaydol tusuna basalim
         driver.findElement(By.xpath("//button[@name='websubmit']")).click();
         bekle(10);
