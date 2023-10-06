da9cbfae0b474b8da0e6264b75adf71d1f65eb5b
sadik Goden <sadikgoden001@gmail.com>
2023-10-06 13:41:33 +0300

    qwert

--- src/test/java/day10_actionClass/C02_dragAndDrop.java
+++ src/test/java/day10_actionClass/C02_dragAndDrop.java
@@ -1,4 +1,28 @@
 package day10_actionClass;
 
-public class C02_dragAndDrop {
+import Utilities.TestBase;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class C02_dragAndDrop extends TestBase {
+    @Test
+    public void test01(){
+        //1- https://demoqa.com/droppable adresine gidelim
+        driver.get("https://demoqa.com/droppable");
+
+        //2- “Drag me” butonunu tutup “Drop here” kutusunun ustune birakalim
+        WebElement suruklenecekElement= driver.findElement(By.xpath("//div[@id='draggable']"));
+        WebElement suruklenecekAlan = driver.findElement(By.xpath("//*[@id='droppable'][1]"));
+        Actions actions =new Actions(driver);
+        actions.dragAndDrop(suruklenecekElement,suruklenecekAlan).perform();
+        //3- “Drop here” yazisi yerine “Dropped!” oldugunu test edin
+        String expectedYazi="Dropped!";
+        String actualYazi = driver.findElement(By.xpath("//*[text()='Dropped!']")).getText();
+        Assert.assertEquals(actualYazi,expectedYazi);
+        bekle(2);
+
+    }
 }
